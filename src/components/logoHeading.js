fd1aa73e336ea0c463ed4f59986f5993cd51e5d0
Ishmaam Khan <extroonie@proton.me>
2024-01-03 16:00:23 +0600

    feat: refactor a few things

--- src/components/logoHeading.js
+++ src/components/logoHeading.js
@@ -9,6 +9,8 @@ import * as React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 
+import { rhythm } from '../util/typography';
+
 const LogoHeading = () => {
 	const data = useStaticQuery(graphql`
 		query BioQuery {
@@ -34,7 +36,7 @@ const LogoHeading = () => {
 				fixed={data.avatar.childImageSharp.fixed}
 				alt={author}
 				style={{
-					marginRight: 1 / 2,
+					marginRight: rhythm(1 / 2),
 					marginBottom: 0,
 					minWidth: 50,
 				}}
--- src/pages/writing.js
+++ src/pages/writing.js
@@ -61,9 +61,7 @@ export default WritingIndex;
 export const Head = () => (
 	<Seo
 		title="My Writing"
-		description={smartypants(
-			`I wouldn't exactly call it a blog, given the lack of a regular posting schedule, but there are times when I put pen to paper (or fingers to keys), and the result is worth sharing. You might find something enjoyable in one of these musings.`,
-		)}
+		description="I wouldn't exactly call it a blog, given the lack of a regular posting schedule, but there are times when I put pen to paper (or fingers to keys), and the result is worth sharing. You might find something enjoyable in one of these musings."
 	/>
 );
 
